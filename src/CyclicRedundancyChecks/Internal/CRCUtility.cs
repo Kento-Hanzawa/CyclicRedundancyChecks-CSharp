ce3bd1cc28c7e73dd3cc22feb267937283564e87
DESKTOP-EK1HCQK\khanz <k.hanzawa.1213@gmail.com>
2021-10-10 14:41:10 +0900

    CRC計算コアをCRCUtilityの部分実装へ移動。 それに伴い、CRCの実装を大幅に改修。

--- src/CyclicRedundancyChecks/Internal/CRCUtility.cs
+++ src/CyclicRedundancyChecks/Internal/CRCUtility.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
 
-namespace CyclicRedundancyChecks
+namespace CyclicRedundancyChecks.Internal
 {
     internal static partial class CRCUtility
     {
